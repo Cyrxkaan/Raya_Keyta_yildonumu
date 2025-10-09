c580d4b2625540e2c289367b153661fdf9630b6b
Kaan <official.Kaan46@gmail.com>
2025-10-09 18:31:56 +0300

    son sürüm

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ceebd90043c865ffc807a874a01e9585
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8bc2a9f9d7b411ef4ac19f259739b156",
+"flutter_bootstrap.js": "bc6d09e8f8b6aeee01c392112f544934",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
